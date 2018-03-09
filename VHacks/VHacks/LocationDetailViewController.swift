3f7607d5c4014fffd1208eaef4def28b98199ec9
Jack Frysinger <frysinger.j@husky.neu.edu>
2018-03-09 13:12:40 +0100

    Add location detail UI and controller

---
+++ VHacks/VHacks/LocationDetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  LocationDetailViewController.swift
+//  VHacks
+//
+//  Created by Jack Frysinger on 3/9/18.
+//  Copyright © 2018 VHacks. All rights reserved.
+//
+
+import UIKit
+
+class LocationDetailViewController: UIViewController {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var costLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func goBack(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
