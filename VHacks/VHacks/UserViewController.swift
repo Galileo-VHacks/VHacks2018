5fe38f34f89897488b06a8bd271e720e0e83ebd7
Jack Frysinger <frysinger.j@husky.neu.edu>
2018-03-09 13:01:42 +0100

    Add basic UI and ViewController classes

---
+++ VHacks/VHacks/UserViewController.swift
@@ -0,0 +1,38 @@
+//
+//  UserViewController.swift
+//  VHacks
+//
+//  Created by Jack Frysinger on 3/9/18.
+//  Copyright © 2018 VHacks. All rights reserved.
+//
+
+import UIKit
+
+class UserViewController: UIViewController {
+
+    @IBOutlet weak var tokensLabel: UILabel!
+    @IBOutlet weak var recentActivityView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
